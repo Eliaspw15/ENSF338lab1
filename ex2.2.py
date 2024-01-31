dbeee8e709461c2efb34fe7486ba3258429da85b
Elias <Elias.poitras@gmail.com>
2024-01-30 20:47:43 -0700

    cleaned up code and added comments

--- ex2.2.py
+++ ex2.2.py
@@ -1,25 +1,20 @@
-consonants = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWZ"
+consonants = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWZ" #initialized all consonants
 
-line_array = []
-#need to start recording once CHAPTER 1. Loomings
+line_array = [] #line
+#need to start recording once CHAPTER 1. Loomings which is at position 843 so 842 since starting at 0
 with open ('pg2701.txt') as my_file:
     line_array = my_file.readlines()
 
-counter = 0
 consonant = 0
 avg = 0
 words = 0
 for i in range(len(line_array)):
-    if i >= 843:
-        counter += 1
-        count = 0
-        for x in range(len(line_array[i])):
-            if line_array[i][count] in consonants:
+    if i >= 842: #finds line 842 then starts operation
+        for x in range(len(line_array[i])): #goes through each charachter in string
+            if line_array[i][x] in consonants: #if consonant increase count
                 consonant+=1            
-            if line_array[i][count] == " ":
+            if line_array[i][x] == " ": #space indicates the word count 
                 words += 1
-            count += 1 
-
 
 avg = consonant/words
 
