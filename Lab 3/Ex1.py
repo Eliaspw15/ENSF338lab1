e1aa6e3b80e7834c9571c07f70e99ed61f928f1c
SamHawq <samiul1004@gmail.com>
2024-02-11 13:03:53 -0700

    update

--- Lab 3/Ex1.py
+++ Lab 3/Ex1.py
@@ -1,20 +1,38 @@
-def merge_sort(arr, low, high)
+def merge_sort(arr, low, high):
     if low < high:
             mid = (low+high)//2
             merge_sort(arr, low, mid)       #low half array
             merge_sort(arr, mid+1, high)    #high half array
             merge(arr, low, mid, high)      #merge the two halves
+            
 
-def merge(a_arr, a_low, a_mid, a_high):     
-      
-      merge_arr = a_arr[]
+def merge(arr, low, mid, high):     #chatgpt 3.5 was used to understand the tracking of the array and better understand the recursive nature of the merge_sort function and how and when it actually used the merge function
+    low_half = arr[low:mid + 1]
+    high_half = arr[mid + 1:high + 1]
 
-      #at the last iter, this should read 2 values
-      #read which is higher
-      #merge them in order in a new (temp) array of 2
-      #return that ordered array of 2
-      #future iters with more than two values need to 
-      #compare say an array of 3 with an array of 2 and
-      #place elements of that both arrays in order but how? 
+    i = j = 0
+    k = low
 
-      #need to track on paper to figure out
+    while i < len(low_half) and j < len(high_half):
+        if low_half[i] <= high_half[j]: 
+            arr[k] = low_half[i]
+            i += 1
+        else:
+            arr[k] = high_half[j]
+            j += 1
+        k += 1
+
+    while i < len(low_half):
+        arr[k] = low_half[i]
+        i += 1
+        k += 1
+
+    while j < len(high_half):
+        arr[k] = high_half[j]
+        j += 1
+        k += 1
+
+test_arr = [8, 42, 25, 3, 3, 2, 27, 3]
+merge_sort(test_arr, 0, 7)
+print(test_arr)
+
