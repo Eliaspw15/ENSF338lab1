3a909e53d11707e1cdfd877fd208c90b2dc359a6
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2024-03-31 14:47:31 -0600

    ex3 and ex5

--- Lab 8/ex3.py
+++ Lab 8/ex3.py
@@ -1,7 +1,80 @@
 #Pseudo to work off of
-Kruskal(weighted connected undirected graph ):
-    tree = null;
-    edges = sequence of all edges of graph sorted by weight;
-    for (i = 1; i ≤ |E| and |tree| < |V|–1; i++)
-            if ei from edges does not form a cycle with edges in tree
-            add ei to tree;
+#Kruskal(weighted connected undirected graph ):
+#    tree = null;
+#    edges = sequence of all edges of graph sorted by weight;
+#    for (i = 1; i ≤ |E| and |tree| < |V|–1; i++)
+#            if ei from edges does not form a cycle with edges in tree
+#            add ei to tree;
+
+class Graph: 
+    def __init__(self):
+        self.graph = {}
+
+    def add_node(self, node):
+        if node not in self.graph:
+            self.graph[node] = []
+
+    def add_edge(self, node1, node2, weight):
+        self.graph[node1].append((node2, weight))
+        self.graph[node2].append((node1, weight))
+
+    def find_parent(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find_parent(parent, parent[i])
+
+    def union(self, parent, rank, x, y):    #chatgpt assisted
+        x_root = self.find_parent(parent, x)
+        y_root = self.find_parent(parent, y)
+
+        if rank[x_root] < rank[y_root]:
+            parent[x_root] = y_root
+        elif rank[x_root] > rank[y_root]:
+            parent[y_root] = x_root
+        else:
+            parent[y_root] = x_root
+            rank[x_root] += 1
+
+    def kruskal(self):
+        result = Graph()
+        parent = {}
+        rank = {}
+        edges = []
+
+        for node, neighbors in self.graph.items():
+            for neighbor, weight in neighbors:
+                edges.append((weight, node, neighbor))
+
+        edges.sort() 
+
+        for node in self.graph:
+            parent[node] = node
+            rank[node] = 0
+
+        # Kruskal's algorithm, chatgpt assisted
+        for weight, node1, node2 in edges:
+            x = self.find_parent(parent, node1)
+            y = self.find_parent(parent, node2)
+
+            if x != y:
+                result.add_node(node1)
+                result.add_node(node2)
+                result.add_edge(node1, node2, weight)
+                self.union(parent, rank, x, y)
+
+        return result
+
+# Example usage:
+graph = Graph()
+graph.add_node(0)
+graph.add_node(1)
+graph.add_node(2)
+graph.add_node(3)
+graph.add_edge(0, 1, 10)
+graph.add_edge(0, 2, 6)
+graph.add_edge(0, 3, 5)
+graph.add_edge(1, 3, 15)
+graph.add_edge(2, 3, 4)
+
+mst_graph = graph.kruskal()
+print(mst_graph.graph)
---
+++ Lab 8/ex5.py
@@ -0,0 +1,126 @@
+#1
+#Topological sorting can be implemented using Depth First Search (DFS).
+#DFS is effective in implementing topological sorting because topological
+#sorting requires visiting nodes in sequential order so that if there is 
+#a directed edge between node A and node B, node A should maintain a position
+#before node B. Since DFS searches down to a leaf node before, backtracking,
+#it can be used effectively for a topological sorting method.
+
+import re
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_node(self, node):
+        if node not in self.graph:
+            self.graph[node] = []
+
+    def add_edge(self, node1, node2, weight):
+        if node1 in self.graph and node2 in self.graph:
+            self.graph[node1].append((node2, weight))
+        else:
+            print("Could not find one of the nodes in the graph. "
+                  "Make sure when you add an edge, you have the nodes already.")
+    
+    #deleted node removal and reading from file for simplicity
+
+#2
+    def isdag(self):        #chatgpt assisted
+        visited = set()
+        stack = set()
+        cycle = [False]  
+
+        def dfs(node):
+            if node in stack:
+                cycle[0] = True
+                return
+            if node in visited:
+                return
+
+            visited.add(node)
+            stack.add(node)
+            for neighbor, _ in self.graph.get(node, []):
+                dfs(neighbor)
+            stack.remove(node)
+
+        for node in self.graph:
+            if not cycle[0]:
+                dfs(node)
+            else:
+                return False
+        return True
+
+#3
+    def toposort(self):     #chatgpt assisted in combining the DFS and checking if visited process
+        if not self.isdag():
+            return None
+
+        visited = set()
+        stack = []
+
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for neighbor, _ in self.graph.get(node, []):
+                dfs(neighbor)
+            stack.append(node)
+
+        for node in self.graph:
+            dfs(node)
+
+        return stack[::-1]
+
+#USAGE
+# usage dag=true, toposort works
+g = Graph()
+
+g.add_node(0)
+g.add_node(1)
+g.add_node(2)
+g.add_node(3)
+g.add_node(4)
+g.add_node(5)
+
+g.add_edge(5, 0, 1)
+g.add_edge(4, 0, 1)
+g.add_edge(4, 1, 1)
+g.add_edge(5, 2, 1)
+g.add_edge(3, 1, 1)
+g.add_edge(2, 3, 1)
+
+if g.isdag():
+    print("The graph is a DAG.")
+
+    topological_order = g.toposort()
+    print("Topological order:", topological_order)
+else:
+    print("The graph contains cycles. Topological sorting is not possible.")
+
+# usage dag=false, toposort says graph contains cycles
+g2 = Graph()
+
+g2.add_node(0)
+g2.add_node(1)
+g2.add_node(2)
+g2.add_node(3)
+g2.add_node(4)
+g2.add_node(5)
+
+# Add edges between nodes without creating cycles
+g2.add_edge(5, 0, 1)
+g2.add_edge(0, 5, 1)    #added one cycle
+g2.add_edge(4, 1, 1)
+g2.add_edge(5, 2, 1)
+g2.add_edge(3, 1, 1)
+g2.add_edge(2, 3, 1)
+
+# Check if the graph is a Directed Acyclic Graph (DAG)
+if g2.isdag():
+    print("The graph is a DAG.")
+
+    # Perform topological sorting
+    topological_order = g2.toposort()
+    print("Topological order:", topological_order)
+else:
+    print("The graph contains cycles. Topological sorting is not possible.")
